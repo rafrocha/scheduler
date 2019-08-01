5bae0088feb592042dafa3f0ac522f7805e1f5d3
Raf <rafaelmarianorocha@gmail.com>
2019-07-31 22:40:11 -0700

    Base button implementation and storybook testing.

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,12 +3,25 @@ import React from "react";
 import "components/Application.scss";
 
 export default function Application(props) {
-  return (
-    <main className="layout">
-      <section className="sidebar" />
-      <section className="schedule">
-        <section className="schedule" />
-      </section>
-    </main>
-  );
+    return (
+        <main className="layout">
+            <section className="sidebar">
+                <img
+                    className="sidebar--centered"
+                    src="images/logo.png"
+                    alt="Interview Scheduler"
+                />
+                <hr className="sidebar__separator sidebar--centered" />
+                <nav className="sidebar__menu" />
+                <img
+                    className="sidebar__lhl sidebar--centered"
+                    src="images/lhl.png"
+                    alt="Lighthouse Labs"
+                />
+            </section>
+            <section className="schedule">
+                <section className="schedule" />
+            </section>
+        </main>
+    );
 }
